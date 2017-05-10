22f9dc69b60f2f7982dfdfacc22eca1ed9838bdb
Giovanni Panasiti <giova.panasiti@gmail.com>
2017-05-10 20:47:30 +0200

    fix create in topic_controller

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -7,15 +7,16 @@ defmodule Discuss.TopicController do
   #   render conn, "index.html"
   # end
 
-  def new(conn, params) do
+  def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
     render conn, "new.html", changeset: changeset
   end
 
-  def create(conn, params) do
-    IO.inspect(params) # result: %{"_csrf_token" => "ICt6Cy8lHEcDHzMZbxcGDkAMKFMnEAAAug5YCTIuyiPtXPcgreFgDA==","_utf8" => "✓", "topic" => %{"title" => "asdasda"}}
-    %{"topic" => topic} = params
+  def create(conn, %{"topic" => topic}) do #  %{"topic" => topic} = params this happens where the argoment should be pattern but we do pattern matching
+    # IO.inspect(params) # result: %{"_csrf_token" => "ICt6Cy8lHEcDHzMZbxcGDkAMKFMnEAAAug5YCTIuyiPtXPcgreFgDA==","_utf8" => "✓", "topic" => %{"title" => "asdasda"}}
+    # we don't have params anymore as the second argument cause we did pattern matching
+    
   end
 
 end
