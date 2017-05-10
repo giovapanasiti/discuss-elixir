312ed1340034f83de1fa05f1835b7de79d642a51
Giovanni Panasiti <giova.panasiti@gmail.com>
2017-05-10 20:53:17 +0200

    fix db insertion

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -9,14 +9,18 @@ defmodule Discuss.TopicController do
 
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
-
+    # the chainset will contain the data the user is trying to insert in the database
     render conn, "new.html", changeset: changeset
   end
 
   def create(conn, %{"topic" => topic}) do #  %{"topic" => topic} = params this happens where the argoment should be pattern but we do pattern matching
     # IO.inspect(params) # result: %{"_csrf_token" => "ICt6Cy8lHEcDHzMZbxcGDkAMKFMnEAAAug5YCTIuyiPtXPcgreFgDA==","_utf8" => "✓", "topic" => %{"title" => "asdasda"}}
     # we don't have params anymore as the second argument cause we did pattern matching
-    
+
+    # we are going to create a changeset
+    changeset = Topic.changeset(%Topic{}, topic)
+    # insertion in the database
+    Repo.insert(changeset)
   end
 
 end
