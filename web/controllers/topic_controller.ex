9d14e7f560627c595888b5195faeb2a8a8350b13
Giovanni Panasiti <giova.panasiti@gmail.com>
2017-05-10 20:33:13 +0200

    add submission form WIP

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -2,12 +2,20 @@ defmodule Discuss.TopicController do
   # This is used to initialize the controller
   use Discuss.Web, :controller
 
+  alias Discuss.Topic
   # def index(conn, _params) do
   #   render conn, "index.html"
   # end
 
   def new(conn, params) do
-    
+    changeset = Topic.changeset(%Topic{}, %{})
+
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, params) do
+    IO.inspect(params) # result: %{"_csrf_token" => "ICt6Cy8lHEcDHzMZbxcGDkAMKFMnEAAAug5YCTIuyiPtXPcgreFgDA==","_utf8" => "✓", "topic" => %{"title" => "asdasda"}}
+    %{"topic" => topic} = params
   end
 
 end
